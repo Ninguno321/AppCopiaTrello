d7f7a4f28f9bbf568b7f2801a957271837e2aef5
Andrey <asantiago.morales@um.es>
2026-02-24 12:09:12 +0100

    ui

---
+++ gestion_proyectos_ui/src/main/java/umu/pds/gestion_proyectos_ui/App.java
@@ -0,0 +1,13 @@
+package umu.pds.gestion_proyectos_ui;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
