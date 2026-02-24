fc841fb73faeb56ca5bf4f5a980fe3a1c75ef6dd
Andrey <asantiago.morales@um.es>
2026-02-24 13:40:15 +0100

    Ventana inicio prueba de internet + logger

--- gestion_proyectos_ui/src/main/java/umu/pds/gestion_proyectos_ui/App.java
+++ gestion_proyectos_ui/src/main/java/umu/pds/gestion_proyectos_ui/App.java
@@ -1,13 +1,59 @@
 package umu.pds.gestion_proyectos_ui;
 
+import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 /**
  * Hello world!
  *
  */
-public class App 
+public class App extends Application
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    private static final Logger log = LoggerFactory.getLogger(App.class);
+	
+	@Override
+    public void start(Stage stage) throws Exception {
+    	
+        // Inicializar Configuración
+        //Configuracion.setInstancia(new ConfiguracionImpl());
+    	//ControladorApp appController = Configuracion.getInstancia().getControladorApp();
+
+    	// Cargar los datos desde JSON
+    	//try {
+    	//    appController.cargarDatos();
+    	//} catch (IOException e) {
+    	 //   e.printStackTrace();
+    	//}
+        
+        log.debug("Test App");
+        // Cargar vista
+        FXMLLoader loader = new FXMLLoader(
+            getClass().getResource(
+            		"/umu/pds/gestion_proyectos_ui/inicio/VentanaInicio.fxml"
+            )
+        );
+
+        Parent root = loader.load();
+
+        // Inyectar controlador de aplicación
+       // VentanaInicioController viewController = loader.getController();
+        //viewController.setControlador(
+        //    Configuracion.getInstancia().getControladorApp()
+        //);
+
+        stage.setScene(new Scene(root, 1200, 700));
+        stage.setTitle("Gestión de Proyectos");
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
